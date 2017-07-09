3e97a848d2a8b0becd0fdf0a645946fc3faaf3b7
Istabraq <angel.ofmoons@gmail.com>
2017-07-09 09:53:54 -0400

    refactor repreated categories nav code into its own file

--- product_catalog/product_view.php
+++ product_catalog/product_view.php
@@ -1,20 +1,7 @@
 <?php include '../view/header.php'; ?>
 <main>
     <aside>
-        <h1>Categories</h1>
-        <nav>
-            <ul class="nav">
-                <!-- display links for all categories -->
-                <?php foreach($categories as $category) : ?>
-                <li>
-                    <a href="?category_id=<?php 
-                              echo $category['categoryID']; ?>">
-                        <?php echo $category['categoryName']; ?>
-                    </a>
-                </li>
-                <?php endforeach; ?>
-            </ul>
-        </nav>
+      <?php include '../view/categories_nav.php'; ?>
     </aside>
     <section>
         <h1><?php echo $name; ?></h1>
